74e1f1b6aab3f9b367538b5cf2f2d4a5320e9077
VxlentinLR <valentin.lerouge@ngine-innovation.com>
2024-09-27 15:25:35 +0200

    change content and links

--- app/(components)/nav/navbar.tsx
+++ app/(components)/nav/navbar.tsx
@@ -1,22 +1,27 @@
 "use client";
 import Link from "next/link";
 import { ThemeToggle } from "@/app/(components)/nav/theme-toggle";
+import { buttonVariants } from "@/components/ui/button";
 
 export default function Navbar() {
   return (
     <nav className="max-w-[1200px] w-full mx-auto h-[80px] flex items-center justify-between border-b py-5">
       <div className="flex items-center">
-        <h1 className="text-2xl font-bold ml-2">Blog</h1>
+        <Link href="/">
+          <h1 className="text-2xl font-bold ml-2">Blog | Valentin LEROUGE</h1>
+        </Link>
       </div>
       <ul className="flex items-center space-x-5">
         <li>
-          <Link href="/docs">Docs</Link>
-        </li>
-        <li>
-          <Link href="/examples">Examples</Link>
-        </li>
-        <li>
-          <Link href="/blog">Blog</Link>
+          <Link
+            href="https://valentin-lerouge.fr/"
+            target="_blank"
+            className={`border ${buttonVariants({
+              variant: "default",
+            })} w-full md:w-auto`}
+          >
+            Portfolio
+          </Link>
         </li>
         <li>
           <ThemeToggle />
