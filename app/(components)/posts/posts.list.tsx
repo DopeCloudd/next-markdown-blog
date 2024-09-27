88da2027e1e2e3f865b19ab99717e91a9ee59a15
VxlentinLR <valentin.lerouge@ngine-innovation.com>
2024-09-27 15:25:26 +0200

    add style to title

--- app/(components)/posts/posts.list.tsx
+++ app/(components)/posts/posts.list.tsx
@@ -9,7 +9,9 @@ export function PostsList({ posts }: Props) {
   return (
     <section>
       <h2 className="mb-8 text-5xl md:text-7xl font-bold tracking-tighter leading-tight">
-        Les derniers articles
+        <span className="inline bg-gradient-to-r from-[#61DAFB] via-[#1fc0f1] to-[#03a3d7] text-transparent bg-clip-text">
+          Mes derniers articles
+        </span>
       </h2>
       <div className="grid grid-cols-1 md:grid-cols-2 md:gap-x-16 lg:gap-x-32 gap-y-20 md:gap-y-32 mb-32">
         {posts.map((post) => (
