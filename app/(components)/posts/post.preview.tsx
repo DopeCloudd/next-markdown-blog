7e8bf62ff805760e29bd4d3b55ce82df05bb70fc
VxlentinLR <valentin.lerouge@ngine-innovation.com>
2024-09-27 17:23:29 +0200

    add some styles

--- app/(components)/posts/post.preview.tsx
+++ app/(components)/posts/post.preview.tsx
@@ -22,11 +22,11 @@ export function PostPreview({
   slug,
 }: Props) {
   return (
-    <div>
+    <div className="flex flex-col h-full">
       <div className="mb-5">
         <CoverImage slug={slug} title={title} src={coverImage} />
       </div>
-      <h3 className="text-3xl mb-3 leading-snug">
+      <h3 className="text-3xl mb-3 leading-snug min-h-[3rem] flex items-center">
         <Link href={`/posts/${slug}`} className="hover:underline">
           {title}
         </Link>
@@ -34,8 +34,10 @@ export function PostPreview({
       <div className="text-lg mb-4">
         <DateFormatter dateString={date} />
       </div>
-      <p className="text-lg leading-relaxed mb-4">{excerpt}</p>
-      <div className="flex items-center gap-4">
+      <p className="text-lg leading-relaxed mb-4 min-h-[4rem] flex items-center">
+        {excerpt}
+      </p>
+      <div className="flex items-center gap-4 mt-auto">
         <Avatar>
           <AvatarImage src={author.picture} alt={author.name} />
           <AvatarFallback>{author.name}</AvatarFallback>
