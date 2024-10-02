83b019be7e6cc6f3625462663daacabb61ddf875
VxlentinLR <valentin.lerouge@ngine-innovation.com>
2024-10-02 13:22:38 +0200

    MDX Components

---
+++ mdx/mdx-components.tsx
@@ -0,0 +1,9 @@
+import { MdxPre } from "@/mdx/components/MdxPre";
+import { MdxNote } from "@/mdx/components/MdxNote";
+import { BarChartAvantages } from "@/app/(components)/charts/bar";
+
+export const MDX_COMPONENTS = {
+  pre: MdxPre,
+  Note: MdxNote,
+  BarChartAvantages: BarChartAvantages,
+};
